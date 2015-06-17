725832d63e7325cf52369483a9e82e6970e57893
Christopher Aguirre <chris.aguirre11@gmail.com>
2015-06-17 02:08:02 -0500

    Improvements to QuickSort

--- JavaDataStructuresAlgorithmsProjects/src/sorting/QuickSort.java
+++ JavaDataStructuresAlgorithmsProjects/src/sorting/QuickSort.java
@@ -10,33 +10,56 @@ public class QuickSort {
 
 	public static void main(String[] args) {
 //		int[] data = {7, 4, 8, 8, 17, 21, 4, -4, -3, -4, -4, 2, 8};
-		int[] data = {56, 8, 12, 0, 0, 0, 34, -1, 2, -2, -1, -3, 3, 88, 4};
+//		int[] data = {56, 8, 12, 0, 0, 0, 34, -1, 2, -2, -1, -3, 3, 88, 4};
+		int[] data = {7, 4, 8, 8, 17};
 		
-		printArrayContents(data);
-		partition(data, 0, data.length, 9);
-		printArrayContents(data);
+		System.out.println(Arrays.toString(data));
+//		partition(data, 0, data.length);
+		quicksort(data, 0, data.length, 0);
+		System.out.println(Arrays.toString(data));
+		
+	}
+	
+	public static void quicksort(int[] data, int first, int n, int pIndex) {
+		int pivotIndex = 0;
+		int n1;
+		int n2;
+		
+		if(n > 1) {
+			
+			pivotIndex = partition(data, first, n, pIndex);
+			
+			n1 = pivotIndex - first;
+			n2 = n - n1 - 1;
+			
+			quicksort(data, first, n1, pIndex);
+			quicksort(data, pivotIndex+1, n2, pivotIndex+1);
+		}
 	}
 	
 	/**
 	 * Partition the array by making the pivotIndex the median of 3 random array indexes.
 	 *  More Efficient - Good Way to Choose a Pivot
 	 */
-	public static void partition(int[] data, int first, int n) {
+	public static int partition(int[] data, int first, int n) {
 		int pivotIndex = selectMedianIndexFrom3RandomElements(data);
 		int pivotValue = data[pivotIndex];
-		executePartition(data, first, n, pivotIndex, pivotValue);
+		int newPivotIndex = executePartition(data, first, n, pivotIndex, pivotValue);
+		return newPivotIndex;
 	}
 	
 	/**
 	 * Partition the array based on some pre-determined pivotIndex.
 	 *  Not Very Efficient - Bad Way to Choose a Pivot
 	 */
-	public static void partition(int[] data, int first, int n, int pivotIndex) {
+	public static int partition(int[] data, int first, int n, int pivotIndex) {
 		int pivotValue = data[pivotIndex];
-		executePartition(data, first, n, pivotIndex, pivotValue);
+		int newPivotIndex = executePartition(data, first, n, pivotIndex, pivotValue);
+		return newPivotIndex;
 	}
 	
-	private static void executePartition(int[] data, int first, int n,  int pivotIndex, int pivotValue) {
+	private static int executePartition(int[] data, int first, int n,  int pivotIndex, int pivotValue) {
+		System.out.println("Initial PivotIndex is: \t" + pivotIndex);
 		int tooBigIndex = first;
 		int tooSmallIndex = first + n -1;
 		
@@ -46,15 +69,33 @@ public class QuickSort {
 			
 			while(data[tooBigIndex] <= pivotValue)
 				tooBigIndex++;
-			while(data[tooSmallIndex] > pivotValue)
+			while(data[tooSmallIndex] > pivotValue) {
 				tooSmallIndex--;
-			
+			}
+				
+			if( (tooSmallIndex == pivotIndex) && 
+				(tooBigIndex < tooSmallIndex) ) {
+				tooSmallIndex--;
+				continue;
+			}
+				
 			if(tooBigIndex < tooSmallIndex)
 				swapArrayElements(data, tooBigIndex, tooSmallIndex);
 		}
 		
 		swapArrayElements(data, pivotIndex, tooSmallIndex);
 		pivotIndex = tooSmallIndex;
+		System.out.println("New PivotIndex is: \t" + pivotIndex);
+		return pivotIndex;
+		
+//		if(data[tooSmallIndex] < data[tooBigIndex]) {
+//			swapArrayElements(data, pivotIndex, tooBigIndex);
+//			pivotIndex = tooBigIndex;
+//		}
+//		else {
+//			swapArrayElements(data, pivotIndex, tooSmallIndex);
+//			pivotIndex = tooSmallIndex;
+//		}
 	}
 	
 	private static void swapArrayElements(int[] inputArray, int index1, int index2) {
@@ -70,26 +111,21 @@ public class QuickSort {
 		
 		int[] randomIndexesArray = {randomIndex1, randomIndex2, randomIndex3};
 		
-		System.out.println("\nrandomIndexesArray: ");
+		System.out.println("randomIndexesArray: ");
 		for(int n: randomIndexesArray) {
 			System.out.print(n + "\t");
 		}
+		System.out.println();
 		
 		//TODO: Switch to use my own InsertionSort
 		Arrays.sort(randomIndexesArray);
 		
-		System.out.println("\nrandomIndexesArray after sort: ");
+		System.out.println("randomIndexesArray after sort: ");
 		for(int n: randomIndexesArray) {
 			System.out.print(n + "\t");
 		}
+		System.out.println();
 		
 		return randomIndexesArray[1];
-	}
-	
-	private static void printArrayContents(int[] data) {
-		System.out.println("\narrayContents: ");
-		for(int n: data) {
-			System.out.print(n + "\t");
-		}
 	}
 }
--- JavaDataStructuresAlgorithmsProjects/test/sorting/SortingTester.java
+++ JavaDataStructuresAlgorithmsProjects/test/sorting/SortingTester.java
@@ -33,29 +33,36 @@ public class SortingTester {
 		System.out.println("# of Swaps Which Occurred During InsertionSort: " + InsertionSort.numOfSwapsThatOccurred);
 	}
 	
-	private void printArrayContents(int[] array) {
-		System.out.print("[ ");
-		for(int i: array) {
-			System.out.print(array[i] + "\t");
-		}
-		System.out.print(" ]");
-		System.out.println();
-	}
-	
 	@Test
 	public void testQuickSortPartitionMethod() {
+		// Test for PivotIndex = 2
 		int[] arrayToPartition = {7, 4, 8, 8, 17, 21, 4, -4, -3, -4, -4, 2, 8};
-		
+
 		QuickSort.partition(arrayToPartition, 0, arrayToPartition.length, 2);
 		
 		int[] expectedPartitionedArray =  {7, 4, -4, 8, 8, 2, 4, -4, -3, -4, 8, 21, 17};
-		Assert.assertTrue("Array not partitioned as expected", Arrays.equals(expectedPartitionedArray, arrayToPartition));
+		Assert.assertTrue("arrayToPartition not partitioned as expected", Arrays.equals(expectedPartitionedArray, arrayToPartition));
 		
-		int[] arrayToPartition2 = {56, 8, 12, 0, 0, 0, 34, -1, 2, -2, -1, -3, 3, 88, 4};
-		QuickSort.partition(arrayToPartition, 0, arrayToPartition.length, 9);
 		
-		int[] expectedPartitionedArray2 =  {7, 4, -4, 8, 8, 2, 4, -4, -3, -4, 8, 21, 17};
-		Assert.assertTrue("Array not partitioned as expected", Arrays.equals(expectedPartitionedArray2, arrayToPartition2));
+		// Test for PivotIndex = 7
+		int[] arrayToPartition1 = {7, 4, 8, 8, 17, 21, 4, -4, -3, -4, -4, 2, 8};
+		
+		QuickSort.partition(arrayToPartition1, 0, arrayToPartition1.length, 7);
+		
+		int[] expectedPartitionedArray1 =  {-4, -4, 8, 8, 17, 21, 4, -4, -3, 4, 7, 2, 8};
+		Assert.assertTrue("arrayToPartition1 not partitioned as expected", Arrays.equals(expectedPartitionedArray1, arrayToPartition1));
+		
+		
+		// Test for PivotIndex = 6
+		int[] arrayToPartition2 = {7, 4, 8, 8, 17, 21, 4, -4, -3, -4, -4, 2, 8};
+		
+		QuickSort.partition(arrayToPartition2, 0, arrayToPartition2.length, 6);
+		
+		int[] expectedPartitionedArray2 =  {2, 4, -4, -4, -3, -4, 4, 21, 17, 8, 8, 7, 8};
+		Assert.assertTrue("arrayToPartition2 not partitioned as expected", Arrays.equals(expectedPartitionedArray2, arrayToPartition2));
+		
+//		int[] arrayToPartition2 = {56, 8, 12, 0, 0, 0, 34, -1, 2, -2, -1, -3, 3, 88, 4};
+//		QuickSort.partition(arrayToPartition, 0, arrayToPartition.length, 9);
 
 	}
 
