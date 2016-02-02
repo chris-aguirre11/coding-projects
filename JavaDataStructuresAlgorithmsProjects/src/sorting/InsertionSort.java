67ecf44775adaae554247d92c5d6f5d43e495bc5
Christopher Aguirre <chris.aguirre11@gmail.com>
2016-02-02 01:04:12 -0500

    Re-did insertionSort, fully working now. Also added 2 more test scenarios for making sure selectionSort and insertionSort can handle arrays with length=(0/1).

--- JavaDataStructuresAlgorithmsProjects/src/sorting/InsertionSort.java
+++ JavaDataStructuresAlgorithmsProjects/src/sorting/InsertionSort.java
@@ -5,25 +5,23 @@ public class InsertionSort {
 	//Included to show how selectionSort executes much less swaps as compared to insertionSort
 	public static int numOfSwapsThatOccurred = 0;
 	
-	public static void insertionSort(int[] inputArray, int startIndex, int endIndex) {
-		//TODO Do a better job of handling 0 as startIndex
-		if( (startIndex < 1) || (endIndex > inputArray.length))
-			throw new IllegalArgumentException("startIndex or endIndex is invalid!");
+	public static void insertionSort(int[] inputArray, int startIndex, int numElementsToSort) {
+		numOfSwapsThatOccurred = 0;
+		if( (startIndex < 0) || (numElementsToSort < 0) )
+			throw new IllegalArgumentException("startIndex or numElementsToSort cannot be negative!");
 		
-		for(int i = startIndex + 1; i <= endIndex; i++) {
-			for(int y = i; y > 0; y--) {
-				if(inputArray[y] < inputArray[y-1]) {
-					swapArrayElements(y-1, y, inputArray);
+		if( (startIndex + numElementsToSort) > inputArray.length )
+			throw new IllegalArgumentException("Invalid numElementsToSort based on startIndex!");
+		
+		for(int i = startIndex + 1; i < (startIndex + numElementsToSort); i++) {
+			int tmp = inputArray[i];
+			for(int j = i - 1; j >= startIndex; j--) {
+				if(tmp < inputArray[j]) {
+					inputArray[j + 1]= inputArray[j];
+					inputArray[j] = tmp;
+					numOfSwapsThatOccurred++;
 				}
 			}
 		}
 	}
-
-	private static void swapArrayElements(int index1, int index2, int[] inputArray) {
-		int tmp = inputArray[index1];
-		inputArray[index1] = inputArray[index2];
-		inputArray[index2] = tmp;
-		numOfSwapsThatOccurred++;
-	}
-
 }
--- JavaDataStructuresAlgorithmsProjects/test/sorting/SortingTester.java
+++ JavaDataStructuresAlgorithmsProjects/test/sorting/SortingTester.java
@@ -37,17 +37,66 @@ public class SortingTester {
 		int[] expectedSortedArray4 = {-10,-4,-2,8,13,88,4,99,16,-2};
 		Assert.assertTrue(Arrays.equals(expectedSortedArray4, arrayToSort4));
 		System.out.println("# of Swaps Which Occurred During SelectionSort: " + SelectionSort.numOfSwapsThatOccurred);
+		
+		// Scenario: Ensure selectionSort can handle arrays with length = 1
+		int[] arrayToSort5 = {77};
+		SelectionSort.selectionSort(arrayToSort5, 0, 1);
+		int[] expectedSortedArray5 = {77};
+		Assert.assertTrue(Arrays.equals(expectedSortedArray5, arrayToSort5));
+		System.out.println("# of Swaps Which Occurred During SelectionSort: " + SelectionSort.numOfSwapsThatOccurred);
+		
+		// Scenario: Ensure selectionSort can handle arrays with length = 0
+		int[] arrayToSort6 = {};
+		SelectionSort.selectionSort(arrayToSort6, 0, 0);
+		int[] expectedSortedArray6 = {};
+		Assert.assertTrue(Arrays.equals(expectedSortedArray6, arrayToSort6));
+		System.out.println("# of Swaps Which Occurred During SelectionSort: " + SelectionSort.numOfSwapsThatOccurred);
 	}
 	
 	@Test
 	public void testInsertionSort() {
-		int[] arrayToSort = {7,6,3,2,1,14,-1,0};
 		
-		InsertionSort.insertionSort(arrayToSort, 1, 7);
-		
-		int[] expectedSortedArray = {-1,0,1,2,3,6,7,14};
+		// Scenario: Sort index 3 -> End
+		int[] arrayToSort = {7,8,4,-4,10,13,4,9,16,-2};
+		InsertionSort.insertionSort(arrayToSort, 3, 7);
+		int[] expectedSortedArray = {7,8,4,-4,-2,4,9,10,13,16};
 		Assert.assertTrue(Arrays.equals(expectedSortedArray, arrayToSort));
 		System.out.println("# of Swaps Which Occurred During InsertionSort: " + InsertionSort.numOfSwapsThatOccurred);
+		
+		// Scenario: Sort index 0 -> End
+		int[] arrayToSort2 = {3,6,8,1,5,2,7,2};
+		InsertionSort.insertionSort(arrayToSort2, 0, 8);
+		int[] expectedSortedArray2 = {1,2,2,3,5,6,7,8};
+		Assert.assertTrue(Arrays.equals(expectedSortedArray2, arrayToSort2));
+		System.out.println("# of Swaps Which Occurred During InsertionSort: " + InsertionSort.numOfSwapsThatOccurred);
+		
+		// Scenario: Sort index 2 -> index 6
+		int[] arrayToSort3 = {7,8,4,-4,10,13,4,9,16,-2};
+		InsertionSort.insertionSort(arrayToSort3, 2, 5);
+		int[] expectedSortedArray3 = {7,8,-4,4,4,10,13,9,16,-2};
+		Assert.assertTrue(Arrays.equals(expectedSortedArray3, arrayToSort3));
+		System.out.println("# of Swaps Which Occurred During InsertionSort: " + InsertionSort.numOfSwapsThatOccurred);
+		
+		// Scenario: Sort index 0 -> index 5
+		int[] arrayToSort4 = {88,8,-2,-4,-10,13,4,99,16,-2};
+		InsertionSort.insertionSort(arrayToSort4, 0, 6);
+		int[] expectedSortedArray4 = {-10,-4,-2,8,13,88,4,99,16,-2};
+		Assert.assertTrue(Arrays.equals(expectedSortedArray4, arrayToSort4));
+		System.out.println("# of Swaps Which Occurred During InsertionSort: " + InsertionSort.numOfSwapsThatOccurred);
+		
+		// Scenario: Ensure selectionSort can handle arrays with length = 1
+		int[] arrayToSort5 = {77};
+		InsertionSort.insertionSort(arrayToSort5, 0, 1);
+		int[] expectedSortedArray5 = {77};
+		Assert.assertTrue(Arrays.equals(expectedSortedArray5, arrayToSort5));
+		System.out.println("# of Swaps Which Occurred During InsertionSort: " + SelectionSort.numOfSwapsThatOccurred);
+		
+		// Scenario: Ensure selectionSort can handle arrays with length = 0
+		int[] arrayToSort6 = {};
+		InsertionSort.insertionSort(arrayToSort6, 0, 0);
+		int[] expectedSortedArray6 = {};
+		Assert.assertTrue(Arrays.equals(expectedSortedArray6, arrayToSort6));
+		System.out.println("# of Swaps Which Occurred During InsertionSort: " + SelectionSort.numOfSwapsThatOccurred);
 	}
 	
 	@Test
